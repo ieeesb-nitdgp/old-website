611cc1b8b0edc18550bf4ee113be19b64552233d
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-01-15 23:35:14 +0530

    static

--- register/settings.py
+++ register/settings.py
@@ -132,9 +132,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
