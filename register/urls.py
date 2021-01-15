f2500c58353988872998b22debdc6690e4db94d0
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-01-15 23:21:58 +0530

    new settings.py

--- register/urls.py
+++ register/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from django.conf.urls.static import static
 from thereg import views
 from django.conf import settings
 
@@ -29,11 +30,5 @@ urlpatterns = [
 ]
 
 
-from thereg import views
-#from register import settings
-from django.contrib.staticfiles.urls import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
- 
- 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
